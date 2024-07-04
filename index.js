5167040ea9ada05ea1b5a5964910d4b67a9fad36
Tegorinternational <tegorinternational@gmail.com>
2024-07-04 19:05:24 +0530

    Fix issues

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const { Telegraf, Markup } = require('telegraf');
+const app = express();
 const dotenv = require('dotenv');
 
-dotenv.config(); // Load environment variables from .env file
 
-const app = express();
-const port = process.env.PORT || 3000; // Use port 3000 as default
+
+
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
+
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -16,6 +21,7 @@ app.use(cors());
 // Mock user data storage
 let username = '';
 
+
 bot.start((ctx) => {
     username = ctx.from.username;
     ctx.reply(`Hello, ${username}, Choose an option:`, Markup.inlineKeyboard([
@@ -23,28 +29,15 @@ bot.start((ctx) => {
     ]));
 });
 
+
 // Endpoint to get the username
 app.get('/username', (req, res) => {
     res.json({ username });
 });
 
-// Launch bot with webhook in production
-if (process.env.NODE_ENV === 'production') {
-    const webhookUrl = process.env.RENDER_EXTERNAL_URL + '/webhook'; // Adjust as per your Render setup
-    bot.launch({
-        webhook: {
-            domain: webhookUrl,
-            port: port // Use the same port as the Express server
-        }
-    });
-    console.log(`Bot launched with webhook at ${webhookUrl}`);
-} else {
-    // Launch bot in polling mode in development
-    bot.launch();
-    console.log('Bot launched in polling mode');
-}
+bot.launch();
 
-// Start Express server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
