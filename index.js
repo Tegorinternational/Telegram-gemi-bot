af81caca2287a5e1f80ce445f72803a7b1010abd
Tegorinternational <tegorinternational@gmail.com>
2024-07-05 18:27:59 +0530

    fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { Telegraf, Markup } = require('telegraf');
 const app = express();
 const dotenv = require('dotenv');
-
+const localPort = 3000
 
 
 
@@ -20,11 +20,23 @@ app.use(cors());
 
 // Mock user data storage
 let username = '';
+let profilePhotoUrl = '';
 
 
-bot.start((ctx) => {
+bot.start( async (ctx) => {
     username = ctx.from.username;
-    ctx.reply(`Hello, ${username}, Choose an option:`, Markup.inlineKeyboard([
+    const userId = ctx.from.id;
+    
+    const photos = await bot.telegram.getUserProfilePhotos(userId);
+    if (photos.total_count > 0) {
+        const fileId = photos.photos[0][0].file_id;
+        const file = await bot.telegram.getFile(fileId);
+        profilePhotoUrl = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${file.file_path}`;
+    } else {
+        profilePhotoUrl = '';
+    }
+    
+    await ctx.reply(`Hello, ${username}, Choose an option:`, Markup.inlineKeyboard([
         Markup.button.url('Launch', 'https://telegram.me/TgGemiAiBot/TgGemiAiWeb')
     ]));
 });
@@ -32,12 +44,27 @@ bot.start((ctx) => {
 
 // Endpoint to get the username
 app.get('/username', (req, res) => {
-    res.json({ username });
+    res.json({ username, profilePhotoUrl });
 });
 
-bot.launch();
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+
+if (process.env.NODE_ENV === 'production') {
+    const port = process.env.PORT || {localPort};
+    bot.launch({
+        webhook: {
+            domain: process.env.RENDER_EXTERNAL_URL,
+            port: port
+        }
+    });
+    
+    app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+} else {
+    bot.launch();
+    app.listen(localPort, () => {
+    console.log(`Server running on port ${localPort}`);
+});
+}
